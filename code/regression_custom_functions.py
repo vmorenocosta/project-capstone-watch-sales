3ea2c9d4dba36a573a5c0cf5ff6ab7d4ab379ac5
Vitoria Moreno Costa <vitoriamorenocosta@Vitorias-Air-2.attlocal.net>
2022-06-07 16:25:34 -0700

    completed first pass on classification model

--- code/regression_custom_functions.py
+++ code/regression_custom_functions.py
@@ -4,15 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from sklearn.linear_model import LogisticRegression, LinearRegression, RidgeCV, LassoCV
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.pipeline import Pipeline
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, ExtraTreesRegressor
-from sklearn.impute import KNNImputer
+from sklearn.metrics import ConfusionMatrixDisplay
+
 
 from custom_functions import *
 
@@ -49,7 +47,86 @@ def calc_norm_sales(df_models):
     
     return df_models.merge(yearly_sales[['norm_sales']],left_on = 'style_id',right_index=True)
 
+
+def calc_norm_yearly_sales(df_models, last_month_of_sales, year):
+    """ This function calculates a target value - normalized yearly sales, which is calculated by:
+    1. Calculate yearly sales average
+    2. Divide yearly sales by yearly average
+    3. Sum the normalized sales
+    4. Divide the sum by the total # years for which there were sales
+    The function modifies the dataframe in place.
     
+    args:
+        df_models (Pandas dataframe): a dataframe which contains monthly sales
+        last_month_of_sales (int): the last month there were sales in the dataset
+        year (int): the year to use to multiply normalized sales to get target value
+        
+    return:
+        df_models (Pandas dataframe): the dataframe, now containing overall sales and norm_sales
+    """
+    # Calculate overall sales
+    df_models = df_models.merge(pd.DataFrame(return_date_col(df_models.set_index('style_id')).T.sum(),columns=['overall_sales']),left_on = 'style_id',right_index=True)
+
+    # Create new dataframe with yearly sales
+    yearly_sales = return_sales(df_models,'style_id').resample('Y',kind='period').sum().T
+
+    # Calculate the yearly average sales
+    avg_yearly_sales = yearly_sales[yearly_sales > 0].mean()
+
+    # Calculate the total normalized yearly sales (note this number is the total sales divided by the yearly average)
+    total_norm_sales = (yearly_sales/avg_yearly_sales).T.sum()
+
+    # Add this to the dataframe
+    df_models = df_models.merge(pd.DataFrame(total_norm_sales,columns=['total_norm_sales']),left_on = 'style_id',right_index=True)
+
+    # Specify the last month of sales
+    last_month_num = last_month_of_sales # 4 = april
+
+    norm_sales = []
+    for i,x in enumerate(yearly_sales.index):
+        # Calculate number of years of sales for all except the last year
+        years_of_sales = sum([1 for sales in yearly_sales.loc[x] if sales > 0])
+        if yearly_sales.iloc[i,-1] > 0:
+            years_of_sales += last_month_num/12
+        if years_of_sales == 0:
+            norm_sales.append(0)
+        else:
+            # Use 2019 yearly average to convert normalized sales to yearly sales
+            norm_sales.append(avg_yearly_sales[str(year)]*df_models.loc[i,'total_norm_sales']/years_of_sales)
+    yearly_sales['norm_yearly_sales'] = norm_sales
+    
+    plt.title('Normalized yearly sales')
+    plt.hist(norm_sales)
+    plt.ylabel('Num models');
+    
+    return df_models.merge(yearly_sales[['norm_yearly_sales']],left_on = 'style_id',right_index=True)
+
+
+def make_classes(target, class_upper):
+    """ This function assigns a class to items in the dataframe based on a target variable and where it falls in the class upper bounds.
+    
+    args:
+        target (Pandas Series): a series containing the target variable
+        class_upper (dict): a dictionary where the keys are the classes and the values are the upper bounds for the target to the be in the class (inclusive)
+    
+    return:
+        class_sales (list): a list of classes for each target
+    """
+    class_sales = []
+    for i, sales in target.items():
+        c = 0
+        for c in class_upper.keys():
+            if sales <= class_upper[c]:
+                class_sales.append(c)
+                break
+            elif c == max(class_upper.keys()):
+                class_sales.append(c+1)
+
+    plt.hist(class_sales)
+    plt.title('Distribution of labeled classes');
+
+    return class_sales
+
 
 def plot_preds(y_test, preds, model_name):
     """ This function creates two plots: the predictions against the actual values, and the predictions against the residuals.
@@ -135,3 +212,57 @@ def create_evaluate_model(model, X_train, X_test, y_train, y_test):
     
     print()
     return preds, test_cleaned_rmse
+
+
+def create_evaluate_model_class(model, X_train, X_test, y_train, y_test):
+    """ This function accepts X_train, X_test, y_train, y_test, and a regression model instance to preprocess data and create, fit, and evaluate the model.
+    
+    args:
+        model (function): an instance of a classfication model
+        X_train (Pandas dataframe): a dataframe that contains training variables to fit a model
+        X_test (Pandas dataframe): a dataframe that contains testing variables to evaluate a model
+        y_train (Pandas series): a dataframe that contains the training variable to be predicted by the model for evaluation
+        y_test (Pandas series): a dataframe that contains the testing variable to be predicted by the model for evaluation
+    """
+    # Print model name
+    print(model[1])
+    
+    # Create a column transformer to one hot encode categorical variables
+    categorical_attributes = []
+    for attribute in X_train.columns:
+        try:
+            int(attribute)
+        except:
+            categorical_attributes.append(attribute)
+            
+    ct = ColumnTransformer([('ohe',OneHotEncoder(drop='first',sparse=False,handle_unknown='ignore'),
+                         categorical_attributes)],
+                      remainder='passthrough',
+                      verbose_feature_names_out=False)
+    
+    # Create a pipeline to transform, scale, and model
+    pipe = Pipeline([
+        ('ct',ct),
+        ('ss',StandardScaler()),
+        model
+    ])
+
+    # Fit model with training data
+    pipe.fit(X_train,y_train)
+    
+    # Print traw and test score
+    print('Train accuracy:', pipe.score(X_train,y_train))
+    print('Test accuracy:', pipe.score(X_test,y_test))
+
+    # Create predictions with test data
+    preds = pipe.predict(X_test)
+
+    # Evaluate predictions
+    test_accuracy = pipe.score(X_test,y_test)
+    
+    # Generate confusion matrix
+    ConfusionMatrixDisplay.from_predictions(y_test,preds)
+    plt.title(model[0])
+    
+    print()
+    return pipe, preds, test_accuracy
